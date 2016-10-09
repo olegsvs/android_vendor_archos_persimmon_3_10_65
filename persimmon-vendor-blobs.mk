7ac960ff83c4d8e6342b077b1c4bf066418e7d3d
olegsvs <oleg.texet@gmail.com>
2016-10-09 18:48:03 +0000

    sw to persimmon

---
+++ persimmon-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/archos/persimmon/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/archos/persimmon/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/archos/persimmon/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/archos/persimmon/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/archos/persimmon/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/archos/persimmon/vendor,system/vendor)
